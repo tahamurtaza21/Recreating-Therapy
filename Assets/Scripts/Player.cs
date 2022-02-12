6159a5d0f7387b98a2d5b62774051c70f1cb724e
tahamurtaza21 <60212648+tahamurtaza21@users.noreply.github.com>
2022-02-12 14:10:08 +0500

    Changed Level Sprites and working on animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
-
     private float moveAmount;
+    private Animator animator;
+
+    void Start() 
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
     void Update()
     {
         moveAmount = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-
         transform.Translate(moveAmount, 0, 0);
     }
 }
